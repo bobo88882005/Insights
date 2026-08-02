3f7c4cab21733aa51285991fe1f94cc8aab56c02
bobo88882005 <bobo88882005@yahoo.it>
2026-08-02 15:03:52 +0200

    Add MANUAL_EXCLUSIONS set for user exclusions

---
+++ src/utils/exclusions.ts
@@ -0,0 +1,24 @@
+export const MANUAL_EXCLUSIONS = new Set<string>([
+  "_u",
+  "andrea.old",
+  "siro.valmont",
+  "discor_dantes",
+  "matijahefler",
+  "fightingsoul3000",
+  "emilia_lop_",
+  "manfromstel",
+  "bruhlickd",
+  "mario95simonetti",
+  "andreavitto",
+  "cadestellenti",
+  "branlio88",
+  "ut1986",
+  "catsguardian",
+  "iamflaviofarinos",
+  "zacharias_longuelune",
+  "facualbanoinderkum",
+  "riccardoprnd",
+  "achcec28",
+
+  // 👇 Incolla qui il resto della tua lista
+]);
