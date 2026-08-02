bc5c2e9d23f76abf5469b534f6c677264d8239b2
bobo88882005 <bobo88882005@yahoo.it>
2026-08-02 15:18:11 +0200

    Create App component with layout and home page

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import AppLayout from "./components/layout/AppLayout";
+import Home from "./pages/Home";
+
+
+function App() {
+
+  return (
+
+    <AppLayout>
+
+      <Home />
+
+    </AppLayout>
+
+  );
+
+}
+
+
+export default App;
